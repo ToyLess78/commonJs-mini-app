b72659dc1ceaf2635813cb9ca209e403e1a3e525
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-22 15:15:35 +0300

    + add more different es6 imports

--- app.mjs
+++ app.mjs
@@ -1,8 +1,9 @@
-// import { characters, greet }  from './characters.mjs';
-//
-// characters.map(c => greet(c));
+import log,  { characters, greet as hallo }  from './characters.mjs';
 
-import log, * as char  from './characters.mjs';
-
-char.characters.map(c => char.greet(c));
+characters.map(c => hallo(c));
 log();
+
+// import log, * as char  from './characters.mjs';
+//
+// char.characters.map(c => char.greet(c));
+// log();
